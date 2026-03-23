32bed3d70d46bd8c7959c777fe6c2e4fb64cdae3
suraj-laxgan <surajdas11120@gmail.com>
2026-03-23 21:46:21 +0530

    Laravel Breeze authentication and dashboard configure

---
+++ resources/views/components/admin/reponsiveTable.blade.php
@@ -0,0 +1,61 @@
+ <!-- Responsive Table -->
+ <div class="card">
+     <h5 class="card-header">Responsive Table</h5>
+     <div class="table-responsive text-nowrap">
+         <table class="table">
+             <thead>
+                 <tr class="text-nowrap">
+                     <th>#</th>
+                     <th>Table heading</th>
+                     <th>Table heading</th>
+                     <th>Table heading</th>
+                     <th>Table heading</th>
+                     <th>Table heading</th>
+                     <th>Table heading</th>
+                     <th>Table heading</th>
+                     <th>Table heading</th>
+                     <th>Table heading</th>
+                 </tr>
+             </thead>
+             <tbody>
+                 <tr>
+                     <th scope="row">1</th>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                 </tr>
+                 <tr>
+                     <th scope="row">2</th>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                 </tr>
+                 <tr>
+                     <th scope="row">3</th>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                     <td>Table cell</td>
+                 </tr>
+             </tbody>
+         </table>
+     </div>
+ </div>
+ <!--/ Responsive Table -->
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,25 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/health', function () {
-    return response()->json(['status' => 'ok']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/notification', [ProfileController::class, 'notification'])->name('profile.notification');
+    Route::get('/connection', [ProfileController::class, 'connection'])->name('profile.connection');
+
+
 });
+
+require __DIR__.'/auth.php';
